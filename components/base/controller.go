81e05ab52a2ae51a05e671ffb59b9bcf2bda44d6
SHDM\16020028 <dream_zml@163.com>
2019-02-12 18:42:00 +0800

    增加分页控制器公共参数过滤方法

--- components/base/controller.go
+++ components/base/controller.go
@@ -15,7 +15,10 @@
 package base
 
 import (
+	"errors"
 	"time"
+
+	"strings"
 
 	"github.com/astaxie/beego"
 	"github.com/yimishiji/bee/utils"
@@ -35,4 +38,48 @@ func (c *Controller) Resp(appCode utils.ApiCode, msg string, data ...interface{}
 	resp.TimeTaken = c.Ctx.ResponseWriter.Elapsed.Seconds()
 	resp.Time = time.Now().Unix()
 	return resp
+}
+
+func (c *Controller) GetPagePublicParams() (fields []string, sortby []string, order []string, query map[string]string, limit int64, offset int64, err error) {
+	var field []string
+	var sort []string
+	var orders []string
+	var querys = make(map[string]string)
+	var limits int64 = 10
+	var offsets int64
+
+	// fields: col1,col2,entity.col3
+	if v := c.GetString("fields"); v != "" {
+		field = strings.Split(v, ",")
+	}
+
+	// limit: 10 (default is 10)
+	if v, err := c.GetInt64("limit"); err == nil {
+		limits = v
+	}
+	// offset: 0 (default is 0)
+	if v, err := c.GetInt64("offset"); err == nil {
+		offsets = v
+	}
+	// sortby: col1,col2
+	if v := c.GetString("sortby"); v != "" {
+		sort = strings.Split(v, ",")
+	}
+	// order: desc,asc
+	if v := c.GetString("order"); v != "" {
+		orders = strings.Split(v, ",")
+	}
+	// query: k:v,k:v
+	if v := c.GetString("query"); v != "" {
+		for _, cond := range strings.Split(v, ",") {
+			kv := strings.SplitN(cond, ":", 2)
+			if len(kv) != 2 {
+				return field, sort, orders, querys, limits, offsets, errors.New("Error: invalid query key/value pair")
+			}
+			k, v := kv[0], kv[1]
+			querys[k] = v
+		}
+	}
+
+	return field, sort, orders, querys, limits, offsets, err
 }
--- generate/g_appcode.go
+++ generate/g_appcode.go
@@ -1137,9 +1137,7 @@ func Delete{{modelName}}(id int) (err error) {
 import (
 	"{{pkgPath}}/models"
 	"encoding/json"
-	"errors"
 	"strconv"
-	"strings"
 
 	"github.com/yimishiji/bee/components/base"
 	"github.com/yimishiji/bee/utils"
@@ -1213,45 +1211,11 @@ func (c *{{ctrlName}}Controller) GetOne() {
 // @Failure 403
 // @router / [get]
 func (c *{{ctrlName}}Controller) GetAll() {
-	var fields []string
-	var sortby []string
-	var order []string
-	var query = make(map[string]string)
-	var limit int64 = 10
-	var offset int64
+	fields, sortby, order, query, limit, offset, err := c.GetPagePublicParams()
 
-	// fields: col1,col2,entity.col3
-	if v := c.GetString("fields"); v != "" {
-		fields = strings.Split(v, ",")
-	}
-	// limit: 10 (default is 10)
-	if v, err := c.GetInt64("limit"); err == nil {
-		limit = v
-	}
-	// offset: 0 (default is 0)
-	if v, err := c.GetInt64("offset"); err == nil {
-		offset = v
-	}
-	// sortby: col1,col2
-	if v := c.GetString("sortby"); v != "" {
-		sortby = strings.Split(v, ",")
-	}
-	// order: desc,asc
-	if v := c.GetString("order"); v != "" {
-		order = strings.Split(v, ",")
-	}
-	// query: k:v,k:v
-	if v := c.GetString("query"); v != "" {
-		for _, cond := range strings.Split(v, ",") {
-			kv := strings.SplitN(cond, ":", 2)
-			if len(kv) != 2 {
-				c.Data["json"] = c.Resp(utils.ApiCode_ILLEGAL_ERROR, "illegal operation", errors.New("Error: invalid query key/value pair"))
-				c.ServeJSON()
-				return
-			}
-			k, v := kv[0], kv[1]
-			query[k] = v
-		}
+	if err != nil {
+		c.Data["json"] = c.Resp(utils.ApiCode_ILLEGAL_ERROR, "illegal operation", err)
+		c.ServeJSON()
 	}
 
 	l, err := models.GetAll{{ctrlName}}(query, fields, sortby, order, offset, limit)
