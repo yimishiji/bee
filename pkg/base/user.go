78f30e26be5f8a4a8a00d2f0c81e4c53ab022ffc
SHDM\16020028 <dream_zml@163.com>
2019-02-17 10:17:49 +0800

    更新获取用户id方法

--- pkg/base/user.go
+++ pkg/base/user.go
@@ -1,17 +1,28 @@
 package base
 
-var UserInstance = new(User)
+import (
+	"encoding/json"
+
+	"github.com/yimishiji/bee/pkg/db"
+)
 
 // 会员基本信息
 type User struct {
-	Id          string
-	AccessToken string
-	UserInfo    UserInfo
+	Id           string
+	AccessToken  string
+	RefreshToken string
+	UserInfo     UserInfo
 }
 
 //是否是游客
-func (u *User) isGuest() bool {
+func (u *User) IsGuest() bool {
 	return u.Id == "" || u.AccessToken == ""
+}
+
+//是否是游客
+func (u *User) GetId() string {
+	u.Login()
+	return u.Id
 }
 
 // 会员附加信息
@@ -35,4 +46,37 @@ type UserInfo struct {
 	UpdatedAt    string `json:"updated_at"`
 	CreatedAt    string `json:"created_at"`
 	BusinessKey  string `json:"business_key"`
+}
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	RefreshToken string `json:"refresh_token"`
+	PlatformName string `json:"platform_name"`
+	PlatformID   int    `json:"platform_id"`
+	UserID       string `json:"user_id"`
+}
+
+//登录
+func (u *User) Login() {
+	if u.AccessToken == "" || u.Id != "" {
+		return
+	}
+
+	userInfoKey := u.AccessToken + ":token"
+	userToken, err := db.Redis.Get(userInfoKey).Result()
+	if err != nil {
+		return
+	}
+
+	token := new(Token)
+	err = json.Unmarshal([]byte(userToken), &token)
+	if err != nil {
+		return
+	}
+
+	u.RefreshToken = token.RefreshToken
+	u.Id = token.UserID
 }
